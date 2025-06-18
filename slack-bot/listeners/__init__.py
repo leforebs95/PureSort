421b0f38aa2d942ebdfd8de8b86ac0fe5b95c747
Brandon Lefore <brandon.lefore@nivalta.com>
2025-06-17 21:10:10 -0400

    Infra folder

--- slack-bot/listeners/__init__.py
+++ slack-bot/listeners/__init__.py
@@ -4,4 +4,4 @@ from listeners import events
 def register_listeners(app):
     # Using assistant middleware is the recommended way.
     assistant.register(app)
-    events.register(app)
+    # events.register(app)
